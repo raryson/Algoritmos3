0bf3d15c4ea22a24b4e4b753010dfc6b4844736c
Raryson Pereira Rost <raryson.rost@gmail.com>
2017-09-19 09:12:15 -0300

    Update t15.c

--- t5/t15.c
+++ t5/t15.c
@@ -7,55 +7,55 @@ int lestring(char s[], int max)
     int i = 0, contador= 0;
     char letra;
 
-    /* max È o tamanho que "s" pode receber. Deve ser passado o tamanho mesmo, 
-       ou seja, se for passado 100 a funÁ„o j· se encarrega de n„o deixar
+    /* max √© o tamanho que "s" pode receber. Deve ser passado o tamanho mesmo, 
+       ou seja, se for passado 100 a fun√ß√£o j√° se encarrega de n√£o deixar
        passar de 99 cars (+1 do finalizador)
      */
     for (i = 0; i < (max - 1); i++) {
-        /* LÍ um ˙nico caractere do teclado */
+        /* L√™ um √∫nico caractere do teclado */
 
         letra = fgetc(stdin);
     
         if((letra >= 'a' && letra <= 'z') || (letra >= 'A' && letra <= 'Z') || (letra == 10) || (letra == 36)){
         	if(letra != 10){
-        		
+        		s[contador] = letra;
         		contador++;	
 			}
 			
 		}
 
 
-        /* Veja RARYSON que se encontrou um ENTER (\n) mas n„o leu nenhum
-         * caractere v·lido (i continua sendo 0), n„o aceita. Precisa ler algo.
-         * Decrementa o i para anular o efeito do i++ do laÁo
-         * e volta no laÁo com continue
+        /* Veja RARYSON que se encontrou um ENTER (\n) mas n√£o leu nenhum
+         * caractere v√°lido (i continua sendo 0), n√£o aceita. Precisa ler algo.
+         * Decrementa o i para anular o efeito do i++ do la√ßo
+         * e volta no la√ßo com continue
          */
         if ((letra == '\n') && (i == 0)) {
             i = i - 1;
             continue;
         }
 
-        /* Agora se leu um enter ("\n") j· tendo lido caracteres v·lidos, 
-         * ent„o o usu·rio terminou de digitar sua string e ela possui ao 
-         * menos um caractere v·lido
+        /* Agora se leu um enter ("\n") j√° tendo lido caracteres v√°lidos, 
+         * ent√£o o usu√°rio terminou de digitar sua string e ela possui ao 
+         * menos um caractere v√°lido
          */
         if (letra == '\n')
             break;
 
-        /* se o caractere lido n„o È ENTER, apenas o coloca no vetor sting */
+        /* se o caractere lido n√£o √© ENTER, apenas o coloca no vetor sting */
      
     
 }
-    /* Sair· do laÁo anterior por dois motivos:
-       a) usu·rio digitou ENTER e tem ao menos um car v·lido
-       b) mesmo usu·rio n„o tendo digitado ENTER, j· se leu caracteres suficientes
+    /* Sair√° do la√ßo anterior por dois motivos:
+       a) usu√°rio digitou ENTER e tem ao menos um car v√°lido
+       b) mesmo usu√°rio n√£o tendo digitado ENTER, j√° se leu caracteres suficientes
        para encher o s, de acordo com o max (para evitar o overflow)
      */
 
     /* Finaliza a string */
-    s[i] = 0;                   // s[i] = '\0'; È a mesma coisa
+    s[i] = 0;                   // s[i] = '\0'; √© a mesma coisa
 
-    /* retorna a quantidade de cars lidos (pode ser ˙til). Ent„o, esta funÁ„o
+    /* retorna a quantidade de cars lidos (pode ser √∫til). Ent√£o, esta fun√ß√£o
        le uma string e retorna o seu tamanho
      */
     return (contador);
